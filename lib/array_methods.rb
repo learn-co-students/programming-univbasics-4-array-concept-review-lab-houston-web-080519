e4b1d95ccc4003b4b88b1e58421aaefbb93e5f01
Kevin Bagnall <bubbagump2020@gmail.com>
2019-07-17 21:57:49 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,14 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  #using Identify Elements in an Array based on their Index in Array Basics Chapter of Programming Convo 4
+  p array.index(value_to_find)
 end
 
 def find_max_value(array)
-  # Add your solution here
+  sorted_array = array.sort!
+  p sorted_array.last
 end
 
 def find_min_value(array)
-  # Add your solution here
+  sorted_array = array.sort!
+  p sorted_array.first
 end
